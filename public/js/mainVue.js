8a8b1dfee9b47c73854719c2bf37a5e222649023
Casper Johansson <cappecs@hotmail.com>
2019-03-16 18:19:20 +0100

    Cant get new quotes multiple times now. Showing courtesy of tags. Hiding winstreak if none.

--- public/js/mainVue.js
+++ public/js/mainVue.js
@@ -8,22 +8,28 @@ var app = new Vue({
         correctStreak: 0,
         correctTotal: 0,
         totalGuesses: 0,
-        guessThisRound: false
+        guessThisRound: false,
+        gettingNewData: false
     },
     async created() {
         this.getNewTranslation();
     },
     methods: {
-        getNewTranslation: function() {
-            axios.get('/api/translate').then((res) => {
-                this.languages = res.data.languages;
-                this.newLanguage = res.data.newLanguage;
-                this.translatedText = res.data.translatedText;
-                this.quote = res.data.quote;
-                this.guessThisRound = false;
-            });
+        getNewTranslation: function () {
+            if (!this.gettingNewData) {
+                this.gettingNewData = true;
+                axios.get('/api/translate').then((res) => {
+                    this.languages = res.data.languages;
+                    this.newLanguage = res.data.newLanguage;
+                    this.translatedText = res.data.translatedText;
+                    this.quote = res.data.quote;
+                    this.guessThisRound = false;
+                }).finally(() => {
+                    this.gettingNewData = false;
+                });
+            }
         },
-        guess: function(guess, language) {
+        guess: function (guess, language) {
             if (!this.guessThisRound) {
                 this.guessThisRound = true;
                 if (guess === this.newLanguage.language) {
